90f4d53ea9f0917fc1cb6e4b4b4bf01fe055f715
GJ Bogaerts <gj@raker.nl>
2020-06-07 18:21:02 +0200

    Multi image upload and geolocation complete

---
+++ ios/Runner/ForMultiImageDONOTUSE.swift
@@ -0,0 +1,9 @@
+//
+//  ForMultiImageDONOTUSE.swift
+//  Runner
+//
+//  Created by Geert-Jan Bogaerts on 07/06/2020.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
